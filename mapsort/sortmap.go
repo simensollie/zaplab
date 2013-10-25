f2ce06cb8412ee607788543141535febaf492b22
Simen Sollie <simen.sollie@gmail.com>
2013-10-25 14:38:42 +0200

    removed mapsort and added sortmap

---
+++ mapsort/sortmap.go
@@ -0,0 +1,38 @@
+package mapsort
+
+import "sort"
+
+type sortedMap struct {
+	m map[string]int
+	s []string
+}
+
+func (sm *sortedMap) Len() int {
+	return len(sm.m)
+}
+
+func (sm *sortedMap) Less(i, j int) bool {
+	return sm.m[sm.s[i]] > sm.m[sm.s[j]]
+}
+
+func (sm *sortedMap) Swap(i, j int) {
+	sm.s[i], sm.s[j] = sm.s[j], sm.s[i]
+}
+
+func SortedKeys(m map[string]int) []string {
+	sm := new(sortedMap)
+	sm.m = m
+	sm.s = make([]string, len(m))
+	i := 0
+	for key, _ := range m {
+		sm.s[i] = key
+		i++
+	}
+	sort.Sort(sm)
+
+	/*var ret [10]string
+	for i:=0;i<10;i++ {
+		ret[i] = sm.s[i]
+	}*/
+	return sm.s
+}
