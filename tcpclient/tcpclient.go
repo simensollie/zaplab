3fbe03f40977dbef09607b5df4a7513e2873f69b
Kristine Vassøy Svaboe <ksvaboe@pitter22.ux.uis.no>
2013-10-26 16:04:01 +0200

    made tcpclient a echo client

--- tcpclient/tcpclient.go
+++ tcpclient/tcpclient.go
@@ -2,6 +2,7 @@ package tcpclient
 
 import (
 	"net"
+	"os"
 	"fmt"
 )
 
@@ -11,10 +12,18 @@ func something() {
 		os.Exit(1)
 	}
 	service := os.Args[1]
-	tcpAddr, err := net.ResolveTCPAddr("tcp" service)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
 	checkError(err)
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err)
+
+	_, err = conn.Write([]byte("It works!"))
+	checkError(err)
+	var buf [512]byte
+	n, err := conn.Read(buf[0:])
+	checkError(err)
+	fmt.Println(string(buf[0:n]))
+	os.Exit(0)
 }
 
 func checkError(err error) {
